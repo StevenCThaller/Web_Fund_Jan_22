278d2a6add4cedfbf62769d8080930530fda9734
Cody Thaller <stevencthaller@gmail.com>
2022-01-25 11:33:29 -0700

    W1D1 Algos Lesson

--- Algos/W1/D2/D2.js
+++ Algos/W1/D2/D2.js
@@ -1,10 +1,17 @@
 // Lesson 1: Predict the Output
+// var: declaration of a variable
+// a: the shortcut that we'll use to access whatever is stored in memory regarding that variable
+// =: assigning the variable some value
 var a = 25;
 a = a - 13;
+// at this point, a has a value of 12
 console.log(a / 2);
 
 a = "hello";
 console.log(a + " hello");
+// OUTPUT: 
+// 6
+// hello hello
 
 
 // Lesson 2: Predict Loops
@@ -14,8 +21,16 @@ for (var i = 0; i < 10; i++) {
 }
 
 console.log("outside of the loop " + i);
+// OUTPUT:
+// 0
+// 4
+// 8
+// outside of the loop 12
+
 
 // Lesson 3: Predict what the code does
+var myNumbers = [1, 3, 5];
+
 function getTotal(arrayOfNumbers) {
 
     var sum = arrayOfNumbers[0];
@@ -29,7 +44,15 @@ function getTotal(arrayOfNumbers) {
 
 }
 
-getTotal([1, 3, 5]);
+getTotal(myNumbers);
+// OUTPUT:
+// the current sum is: 2
+// the current sum is: 5
+// the current sum is: 10
+// the total is: 10
+
 
 // BONUS: Why does the above code say at the end that the total is 10? Is this the answer
 // you would expect?
+// Fix #1: Initialize sum as 0 instead of arrayOfNumbers[0]
+// Fix #2: Initialize i within the for loop as 1 instead of 0
